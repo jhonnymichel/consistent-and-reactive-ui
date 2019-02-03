cfbb12e94d8ddb554e2821fb588bc489ba0a82a2
Jhonny Michel <jhonnymichel@outlook.com>
2019-02-02 21:55:45 -0300

    Updates

---
+++ precache-manifest.e98033774e482c3fb6cff506f833d856.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9471474eb2dd8c488b3b",
+    "url": "/consistent-and-reactive-ui/static/css/main.fd525185.chunk.css"
+  },
+  {
+    "revision": "9471474eb2dd8c488b3b",
+    "url": "/consistent-and-reactive-ui/static/js/main.9471474e.chunk.js"
+  },
+  {
+    "revision": "fdff8b5b9679febefa55",
+    "url": "/consistent-and-reactive-ui/static/css/1.be2f8269.chunk.css"
+  },
+  {
+    "revision": "fdff8b5b9679febefa55",
+    "url": "/consistent-and-reactive-ui/static/js/1.fdff8b5b.chunk.js"
+  },
+  {
+    "revision": "99300bce4e185c6b4bbe",
+    "url": "/consistent-and-reactive-ui/static/js/runtime~main.99300bce.js"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/consistent-and-reactive-ui/static/media/MaterialIcons-Regular.8a9a261c.woff2"
+  },
+  {
+    "revision": "016c14adad95a0e21a2ae4fbc36955c6",
+    "url": "/consistent-and-reactive-ui/static/media/MaterialIcons-Regular.016c14ad.eot"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/consistent-and-reactive-ui/static/media/MaterialIcons-Regular.c38ebd3c.woff"
+  },
+  {
+    "revision": "d41ae58dab60e355ae3872ebf2f1403a",
+    "url": "/consistent-and-reactive-ui/static/media/MaterialIcons-Regular.d41ae58d.ttf"
+  },
+  {
+    "revision": "e825d4466507e6d50509f531efdcf75c",
+    "url": "/consistent-and-reactive-ui/index.html"
+  }
+];
