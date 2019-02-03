346c82787c339271f96dc0077e9ddaa1657dd88f
Jhonny Michel <jhonnymichel@outlook.com>
2019-02-02 22:18:07 -0300

    Updates

---
+++ precache-manifest.0e63903f82a51a6414cf3e28a3ffbfc6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "308d72a49b51871a68cd",
+    "url": "/jhonnymichel/consistent-and-reactive-ui/static/css/main.19e00972.chunk.css"
+  },
+  {
+    "revision": "308d72a49b51871a68cd",
+    "url": "/jhonnymichel/consistent-and-reactive-ui/static/js/main.308d72a4.chunk.js"
+  },
+  {
+    "revision": "4f09cf9cc3d1dc2a6da8",
+    "url": "/jhonnymichel/consistent-and-reactive-ui/static/css/1.1d8687a8.chunk.css"
+  },
+  {
+    "revision": "4f09cf9cc3d1dc2a6da8",
+    "url": "/jhonnymichel/consistent-and-reactive-ui/static/js/1.4f09cf9c.chunk.js"
+  },
+  {
+    "revision": "4210f4b7408e5f3d9fed",
+    "url": "/jhonnymichel/consistent-and-reactive-ui/static/js/runtime~main.4210f4b7.js"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/jhonnymichel/consistent-and-reactive-ui/static/media/MaterialIcons-Regular.8a9a261c.woff2"
+  },
+  {
+    "revision": "016c14adad95a0e21a2ae4fbc36955c6",
+    "url": "/jhonnymichel/consistent-and-reactive-ui/static/media/MaterialIcons-Regular.016c14ad.eot"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/jhonnymichel/consistent-and-reactive-ui/static/media/MaterialIcons-Regular.c38ebd3c.woff"
+  },
+  {
+    "revision": "d41ae58dab60e355ae3872ebf2f1403a",
+    "url": "/jhonnymichel/consistent-and-reactive-ui/static/media/MaterialIcons-Regular.d41ae58d.ttf"
+  },
+  {
+    "revision": "86112991bc6f8a1d61f4ef2f90298484",
+    "url": "/jhonnymichel/consistent-and-reactive-ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/consistent-and-reactive-ui/precache-manifest.ef3b9f37710d1ac31c6d7509ab99c86d.js"
+  "/jhonnymichel/consistent-and-reactive-ui/precache-manifest.0e63903f82a51a6414cf3e28a3ffbfc6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/consistent-and-reactive-ui/index.html", {
+workbox.routing.registerNavigationRoute("/jhonnymichel/consistent-and-reactive-ui/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
